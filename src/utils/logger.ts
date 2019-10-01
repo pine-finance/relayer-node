408d24c9f1d44b586363bf3f675f857b0d1888cc
Ignacio Mazzara <nachomazzara@gmail.com>
2019-10-01 09:37:28 -0300

    feat: winston with days

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,16 +1,27 @@
+
 import { createLogger, Logger, format, transports } from 'winston'
+import DailyRotateFile from "winston-daily-rotate-file"
+
 const { combine, timestamp, printf } = format
 
 const myFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`
 })
 
+var transport = new (DailyRotateFile)({
+  filename: `${process.env.LOGGER_FILE}-%DATE%.log`,
+  datePattern: 'YYYY-MM-DD-HH',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d'
+});
+
 export const logger: Logger = createLogger({
   level: process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info',
   defaultMeta: { service: 'user-service' },
   format: combine(timestamp(), myFormat),
   transports: [
-    new transports.File({ level: 'debug', filename: process.env.LOGGER_FILE }),
+    transport,
     new transports.Console()
   ]
 })
